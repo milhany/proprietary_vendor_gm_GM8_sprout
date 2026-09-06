0bb72d9b59a2b73cf0b931a16b8808ee34a556c3
Muharrem İlhan <158026070+milhany@users.noreply.github.com>
2026-09-06 11:06:56 +0300

    GM8_sprout: fix remaining PRODUCT_COPY_FILES continuations

--- GM8_sprout-vendor.mk
+++ GM8_sprout-vendor.mk
@@ -476,9 +476,7 @@ PRODUCT_COPY_FILES += \
     vendor/gm/GM8_sprout/proprietary/vendor/etc/media_codecs_8937_v1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_8937_v1.xml \
     vendor/gm/GM8_sprout/proprietary/vendor/etc/media_codecs_8956.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_8956.xml \
     vendor/gm/GM8_sprout/proprietary/vendor/etc/media_codecs_dolby_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_dolby_audio.xml \
-
     vendor/gm/GM8_sprout/proprietary/vendor/etc/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
-
     vendor/gm/GM8_sprout/proprietary/vendor/etc/media_codecs_vendor_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_vendor_audio.xml \
     vendor/gm/GM8_sprout/proprietary/vendor/etc/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles.xml \
     vendor/gm/GM8_sprout/proprietary/vendor/etc/media_profiles_8956.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_8956.xml \
